8f105826d02d06e9df6255b5a8ad93698a8564c3
csPython <cspython@AD25TF04NGF1J-17.local>
2019-02-12 11:40:45 -0500

    Added blue triangle to the center of the window

--- shapes5AA.py
+++ shapes5AA.py
@@ -1,1 +1,12 @@
 #shapes5AA.py by akhi abdullah 
+from graphics import*
+
+bTriWin = GraphWin("blue triangle",300,300)
+bTriWin.setCoords(0,0,300,300)
+
+bTri = Polygon (Point(100,100), Point (150,200), Point (200,100))
+bTri.setFill (color_rgb(30,30,230))
+bTri.draw (bTriWin)
+
+bTriWin.getMouse()
+bTriWin.close()
