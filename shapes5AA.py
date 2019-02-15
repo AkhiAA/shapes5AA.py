8ca7a81b1d643a0636b77b7faedba0a232db2f20
csPython <cspython@AD25TF04NGF1J-17.local>
2019-02-15 09:41:39 -0500

    added red circle and green rectangle to shapesWin

--- shapes5AA.py
+++ shapes5AA.py
@@ -1,12 +1,36 @@
 #shapes5AA.py by akhi abdullah 
 from graphics import*
 
-bTriWin = GraphWin("blue triangle",300,300)
-bTriWin.setCoords(0,0,300,300)
+shapesWin = GraphWin("blue triangle",500,500)
+shapesWin.setCoords(90,90,950,950)
 
-bTri = Polygon (Point(100,100), Point (150,200), Point (200,100))
-bTri.setFill (color_rgb(30,30,230))
-bTri.draw (bTriWin)
+bTri = Polygon(Point(100,100), Point(150,200), Point(200,100))
+bTri.setFill(color_rgb(30,30,230))
+bTri.draw(shapesWin)
 
-bTriWin.getMouse()
-bTriWin.close()
+rCircle = Circle(Point(910,910), 30.5)
+rCircle.setFill(color_rgb(230,10,10))
+rCircle.draw(shapesWin)
+
+gRectangle = Rectangle(Point(940,100),Point (850,150))
+gRectangle.setFill(color_rgb(40,230,30))
+gRectangle.draw(shapesWin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+shapesWin.getMouse()
+shapesWin.close()
